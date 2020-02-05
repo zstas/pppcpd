6cf10e586ff7f0f3f242605b21982b26265f28fb
zstas <zstaseg@gmail.com>
2020-02-05 18:25:42 +0000

    remove unnecessary things. now we use boost instead of creating raw sockets by hands

--- src/main.cpp
+++ src/main.cpp
@@ -13,16 +13,6 @@ PPPOEQ ppp_outcoming;
 int main( int argc, char *argv[] ) {
     runtime = std::make_shared<PPPOERuntime>( "pppoe-cp" );
 
-    if( auto const &err = runtime->setupPPPOEDiscovery(); !err.empty() ) {
-        log( "Cannot start pppoe control daemon: " + err );
-        exit( -1 );
-    }
-
-    if( auto const &err = runtime->setupPPPOESession(); !err.empty() ) {
-        log( "Cannot start pppoe control daemon: " + err );
-        exit( -1 );
-    }
-
     // At this point all the config lies here
     runtime->pppoe_conf = std::make_shared<PPPOEPolicy>();
     runtime->pppoe_conf->ac_name = "vBNG AC PPPoE";
@@ -61,75 +51,4 @@ int main( int argc, char *argv[] ) {
     ppp_dispatcher.join();
 
     return 0;
-}
-
-std::string PPPOERuntime::setupPPPOEDiscovery() {
-    struct sockaddr_ll sa;
-
-    if( PPPOEDiscFD = socket( PF_PACKET, SOCK_RAW, htons( ETH_PPPOE_DISCOVERY ) ); PPPOEDiscFD < 0 ) {
-        return "Cannot open socket for PPPOE Discovery packets: "s + strerror( errno );
-    }
-    if( int optval = 1; setsockopt( PPPOEDiscFD, SOL_SOCKET, SO_BROADCAST, &optval, sizeof( optval ) ) < 0 ) {
-        return "Cannot exec setsockopt: "s + strerror( errno );
-    }
-
-    // Handling pppoe discovery packets
-    memset( &sa, 0, sizeof( sa ) );
-    sa.sll_family = AF_PACKET;
-    sa.sll_protocol = htons( ETH_PPPOE_DISCOVERY );
-
-    struct ifreq ifr;
-    memset( &ifr, 0, sizeof( ifr ) );
-    strncpy( ifr.ifr_ifrn.ifrn_name, ifName.c_str(), IFNAMSIZ );
-    ifr.ifr_ifrn.ifrn_name[ IFNAMSIZ - 1 ] = 0;
-
-    if( ioctl( PPPOEDiscFD, SIOCGIFHWADDR, &ifr ) < 0) {
-	    return "ioctl(SIOCGIFHWADDR)";
-	}
-	memcpy( hwaddr.data(), ifr.ifr_hwaddr.sa_data, ETH_ALEN);
-
-    if( ioctl( PPPOEDiscFD, SIOCGIFINDEX, &ifr ) < 0) {
-	    return "Cannot get ifindex for interface";
-    }
-
-    log( "Ifindex: "s + std::to_string( ifr.ifr_ifindex ) );
-    sa.sll_ifindex = ifr.ifr_ifindex;
-
-    if( bind( PPPOEDiscFD, reinterpret_cast<struct sockaddr*>( &sa ), sizeof( sa ) ) < 0 ) {
-        return "Cannot bind on interface: "s + strerror( errno );
-    }
-
-    return "";
-}
-
-std::string PPPOERuntime::setupPPPOESession() {
-    struct sockaddr_ll sa;
-
-    if( PPPOESessFD = socket( PF_PACKET, SOCK_RAW, htons( ETH_PPPOE_SESSION ) ); PPPOESessFD < 0 ) {
-        return "Cannot open socket for PPPOE Discovery packets: "s + strerror( errno );
-    }
-
-    // Handling pppoe session packets
-    memset( &sa, 0, sizeof( sa ) );
-    sa.sll_family = AF_PACKET;
-    sa.sll_protocol = htons( ETH_PPPOE_SESSION );
-
-        struct ifreq ifr;
-    memset( &ifr, 0, sizeof( ifr ) );
-    strncpy( ifr.ifr_ifrn.ifrn_name, ifName.c_str(), IFNAMSIZ );
-    ifr.ifr_ifrn.ifrn_name[ IFNAMSIZ - 1 ] = 0;
-
-    if( ioctl( PPPOEDiscFD, SIOCGIFINDEX, &ifr ) < 0) {
-	    return "Cannot get ifindex for interface";
-    }
-
-    log( "Ifindex: "s + std::to_string( ifr.ifr_ifindex ) );
-    sa.sll_ifindex = ifr.ifr_ifindex;
-
-
-    if( bind( PPPOESessFD, reinterpret_cast<struct sockaddr*>( &sa ), sizeof( sa ) ) < 0 ) {
-        return "Cannot bind on interface: "s + strerror( errno );
-    }
-
-    return "";
 }
