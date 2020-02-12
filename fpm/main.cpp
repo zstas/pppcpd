6dcadae6d6998d33e7977a215a5437af7d9df673
zstas <zstaseg@gmail.com>
2020-02-12 19:53:46 +0000

    started to write fpm interface

---
+++ fpm/main.cpp
@@ -0,0 +1,96 @@
+#include "main.hpp"
+
+struct Netlink {
+
+    static int data_cb_route( const struct nlmsghdr *nlh, void *data ) {
+        struct rtmsg *rm = reinterpret_cast<struct rtmsg *>( mnl_nlmsg_get_payload( nlh ) );
+        
+    }
+
+    static int data_cb( const struct nlmsghdr *nlh, void *data )
+    {
+
+    	switch( nlh->nlmsg_type ) {
+    	case RTM_NEWROUTE:
+    	case RTM_DELROUTE:
+            return data_cb_route(nlh, data);
+    	case RTM_NEWNEIGH:
+    	case RTM_DELNEIGH:
+            std::cout << "NEIGH" << std::endl;
+            break;
+    		//return data_cb_neighbor(nlh, data);
+    	case RTM_NEWADDR:
+    	case RTM_DELADDR:
+            std::cout << "ADDR" << std::endl;
+            break;
+    		//return data_cb_address(nlh, data);
+    	default:
+            break;
+    	}
+        return MNL_CB_OK;
+    }
+
+
+    void process( std::vector<uint8_t> &v) {
+        fpm_msg_hdr_t *hdr;
+        hdr = reinterpret_cast<fpm_msg_hdr_t *>( v.data() );
+        if( hdr->msg_type == FPM_MSG_TYPE_NETLINK ) {
+            auto ret = mnl_cb_run( fpm_msg_data( hdr ), fpm_msg_len( hdr ), 0, 0, data_cb, nullptr );
+            std::cout << "mnl_cb_run: " << ret << std::endl;
+        }
+    }
+};
+
+class FPM_conn {
+private:
+    boost::asio::ip::tcp::socket socket;
+    std::array<uint8_t,1500> buf;
+public:
+    FPM_conn( boost::asio::ip::tcp::socket s ):
+        socket( std::move( s ) )
+    {
+
+    }
+
+    void start() {
+        socket.async_receive( boost::asio::buffer( buf ), std::bind( &FPM_conn::on_read, this, std::placeholders::_1, std::placeholders::_2 ) );
+    }
+
+    void on_read( boost::system::error_code ec, std::size_t length ) {
+        std::cout << "rcd bytes: " << length << std::endl;
+        std::vector<uint8_t> v { buf.data(), buf.data() + length };
+        Netlink nl;
+        nl.process( v );
+        start();
+    }
+};
+
+class FPM_mgr {
+private:
+    boost::asio::io_context io;
+    boost::asio::ip::address addr;
+    boost::asio::ip::tcp::acceptor acceptor;
+    boost::asio::ip::tcp::socket socket;
+
+    std::queue<FPM_conn> connections;
+public:
+    FPM_mgr():
+        addr( boost::asio::ip::address_v4::any() ),
+        acceptor( io, boost::asio::ip::tcp::endpoint( boost::asio::ip::tcp::v4(), 31337 ) ),
+        socket( io )
+    {
+        acceptor.async_accept( socket, std::bind( &FPM_mgr::on_accept, this, std::placeholders::_1 ) );
+        io.run();
+    }
+
+    void on_accept( boost::system::error_code ec ) {
+        auto &it = connections.emplace( std::move( socket ) );
+        it.start();
+        acceptor.async_accept( socket, std::bind( &FPM_mgr::on_accept, this, std::placeholders::_1 ) );
+    }
+};
+
+int main( int argc, char *argv[] ) {
+    FPM_mgr fpm;
+    return 0;
+}
---
+++ fpm/main.hpp
@@ -0,0 +1,8 @@
+#include <boost/asio.hpp>
+#include <iostream>
+#include <queue>
+
+#include <libmnl/libmnl.h>
+#include <linux/rtnetlink.h>
+
+#include "fpm.h"
