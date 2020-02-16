96a4674dac124e17be1beaa386bd5424af57ff6d
zstas <zstaseg@gmail.com>
2020-02-16 13:57:15 +0000

    routes installed and even ecmp is working!

--- fpm/src/vpp.cpp
+++ fpm/src/vpp.cpp
@@ -33,12 +33,13 @@ bool VPPAPI::add_route( fpm::Message &m ) {
     }
 
     auto &add_route = m.add_route();
-    if( !add_route.nexthops()[0].has_address() ) {
+    if( add_route.nexthops().size() < 1 ) {
         log( "Cannot install routes without ip at this moment" );
     }
     if( add_route.address_family() == qpb::AddressFamily::IPV4 ) {
         req.route.table_id = 0;
         req.route.prefix.address.af = vapi_enum_address_family::ADDRESS_IP4;
+        req.is_multipath = 0;
         // Filling up the route info
         auto &prefix = add_route.key().prefix();
         req.route.prefix.address.un.ip4[0] = prefix.bytes()[0];
@@ -46,12 +47,19 @@ bool VPPAPI::add_route( fpm::Message &m ) {
         req.route.prefix.address.un.ip4[2] = prefix.bytes()[2];
         req.route.prefix.address.un.ip4[3] = prefix.bytes()[3];
         req.route.prefix.len = prefix.length();
+        log( std::to_string( prefix.length() ) );
         // Filling up the nexthop info
-        auto &nexthop = add_route.nexthops()[0].address().v4();
-        req.route.paths->nh.address.ip4[0] = ( nexthop.value() & 0xFF000000 ) >> 24;
-        req.route.paths->nh.address.ip4[1] = ( nexthop.value() & 0x00FF0000 ) >> 16;
-        req.route.paths->nh.address.ip4[2] = ( nexthop.value() & 0x0000FF00 ) >> 8;
-        req.route.paths->nh.address.ip4[3] = ( nexthop.value() & 0x000000FF );
+        auto &nexthops = add_route.nexthops();
+        req.route.n_paths = nexthops.size();
+        uint8_t i = 0;
+        for( auto const &nh: nexthops ) {
+            auto &nexthop = nh.address().v4();
+            req.route.paths[i].nh.address.ip4[0] = ( nexthop.value() & 0xFF000000 ) >> 24;
+            req.route.paths[i].nh.address.ip4[1] = ( nexthop.value() & 0x00FF0000 ) >> 16;
+            req.route.paths[i].nh.address.ip4[2] = ( nexthop.value() & 0x0000FF00 ) >> 8;
+            req.route.paths[i].nh.address.ip4[3] = ( nexthop.value() & 0x000000FF );
+            i++;
+        }
     } else if( add_route.address_family() == qpb::AddressFamily::IPV6 ) {
 
     } else {
