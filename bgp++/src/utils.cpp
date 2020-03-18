6723fb1b9f82d256b0cea58ccbb3f5d4f16a0888
zstas <zstaseg@gmail.com>
2020-03-18 19:36:33 +0000

    some log enhancenments

--- bgp++/src/utils.cpp
+++ bgp++/src/utils.cpp
@@ -1,5 +1,33 @@
 #include "main.hpp"
+#include <chrono>
+#include <ctime>
+#include <sstream>
+#include <iomanip>
+
+std::ostream& operator<<( std::ostream & os, const LOG_APP & app ) {
+    switch( app ) {
+    case LOG_APP::APPLICATION:
+        os << "[APPLICATION]: ";
+        break;
+    case LOG_APP::FSM:
+        os << "[FSM]: ";
+        break;
+    case LOG_APP::CONNECTION:
+        os << "[CONNECTION]: ";
+        break;
+    case LOG_APP::CONFIGURATION:
+        os << "[CONFIGURATION]: ";
+        break;
+    }
+    return os;
+}
 
 void log( const std::string &msg ) {
-    std::cout << msg << std::endl;
+    log( LOG_APP::APPLICATION, msg );
+}
+
+void log( const LOG_APP &app, const std::string &msg ) {
+    auto in_time_t = std::chrono::system_clock::to_time_t( std::chrono::system_clock::now() );
+
+    std::cout << std::put_time( std::localtime( &in_time_t ), "%Y-%m-%d %X ") << app << msg << std::endl;
 }
--- bgp++/src/utils.hpp
+++ bgp++/src/utils.hpp
@@ -1,6 +1,14 @@
 #ifndef UTILS_H_
 #define UTILS_H_
 
+enum class LOG_APP {
+    APPLICATION,
+    FSM,
+    CONNECTION,
+    CONFIGURATION,
+};
+
 void log( const std::string &msg );
+void log( const LOG_APP &app, const std::string &msg );
 
 #endif
