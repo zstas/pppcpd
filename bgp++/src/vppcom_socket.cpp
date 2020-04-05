357474c073f3f519f69e48568f360b19b6628dd3
zstas <zstaseg@gmail.com>
2020-04-05 15:13:32 +0000

    vppcom asio service part2: op queue

--- bgp++/src/vppcom_socket.cpp
+++ bgp++/src/vppcom_socket.cpp
@@ -7,7 +7,7 @@ vppcom_service::vppcom_service( io_context &ctx ):
 {
     auto ret = vppcom_app_create( "bgp++ vppcom" );
     log( "app_create: "s + std::to_string( ret ) );
-    fd = vppcom_epoll_create();
+    epoll_fd = vppcom_epoll_create();
 }
 
 vppcom_service::~vppcom_service() {
@@ -22,17 +22,55 @@ void vppcom_service::start() {
 
 void vppcom_service::run_epoll() {
     struct epoll_event vcl_events[ 10 ];
-    int num = vppcom_epoll_wait( fd, vcl_events, 10, 0 );
+    int num = vppcom_epoll_wait( epoll_fd, vcl_events, 10, 0 );
     if( num < 0 ) {
         log( "Error on vppcom_epoll_wait" );
         return;
     }
+
     for( auto const &ev: vcl_events ) {
-        auto event_fd = ev.data.u32;
+        auto event_fd = ev.data.fd;
         if( ev.events & EPOLLIN ) {
-            async_io_service.post();
+            process_events( event_fd, op_type::READ );
+        }
+        if( ev.events & EPOLLOUT ) {
+            process_events( event_fd, op_type::WRITE );
+        }
+        if( ev.events & EPOLLHUP ) {
+            process_events( event_fd, op_type::ERROR );
+        }
+            if( auto const &listener = listeners.find( event_fd ); listener != listeners.end() ) {
+                vppcom_endpt_t peer_endpt;
+                auto new_session = vppcom_session_accept( event_fd, &peer_endpt, O_NONBLOCK );
+                sessions.try_emplace( event_fd, event_fd, listener->second.local, peer_endpt );
+
+                struct epoll_event event;
+                memset( &event, 0, sizeof( event ) );
+                event.events = EPOLLIN | EPOLLOUT;
+                event.data.u32 = new_session;
+
+                auto ret = vppcom_epoll_ctl( epoll_fd, EPOLL_CTL_ADD, new_session, &event );
+                if( ret != VPPCOM_OK ) {
+                    log( "Cannot add event to epoll" );
+                }
+                
+            }
+    }
+}
+
+void vppcom_service::process_events( uint32_t fd, op_type op ) {
+    for( auto const &ev: scheduled_ops ) {
+        if( ev.socket == fd && ev.type == op ) {
+            ev.func(  );
+            return;
         }
     }
+    incoming_ops.emplace_back( fd, op );
+}
+
+void vppcom_listener::async_accept( accept_handler func ) {
+    //io.post();
+    
 }
 
 void vppcom_worker::callback( boost::system::error_code &ec ) {
@@ -40,21 +78,3 @@ void vppcom_worker::callback( boost::system::error_code &ec ) {
         log( "Error on async wait: "s + ec.message() );
     }
 }   
-
-vppcom_socket::vppcom_socket( uint16_t port ) {
-    socket_handler = vppcom_session_create( 0, 1 );
-    log( "session_create: "s + std::to_string( socket_handler ) );
-    vppcom_endpt_t enp;
-    uint32_t addr = 0;
-    enp.parent_handle = 0;
-    enp.ip = reinterpret_cast<uint8_t*>( &addr );
-    enp.is_ip4 = 1;
-    enp.port = bswap16( port );
-    enp.is_cut_thru = 0;
-
-    int ret = vppcom_session_bind( socket_handler, &enp );
-    log( "session_bind: "s + std::to_string( ret ) );
-
-    ret = vppcom_session_listen( socket_handler, 0 );
-    log( "session_listen: "s + std::to_string( ret ) );
-}
--- bgp++/src/vppcom_socket.hpp
+++ bgp++/src/vppcom_socket.hpp
@@ -1,3 +1,25 @@
+using pkt_queue = std::deque<std::vector<uint8_t>>;
+
+typedef void *accept_handler(boost::system::error_code);
+
+enum class op_type : uint8_t {
+    READ,
+    WRITE,
+    ERROR
+};
+
+struct op {
+    op_type type;
+    uint32_t socket;
+    std::function<void( boost::system::error_code &ec, ssize_t len )> func;
+
+    op() = default;
+    op( uint32_t s, op_type t):
+        socket( s ),
+        type( t )
+    {}
+};
+
 struct vppcom_service : public io_context::service {
     explicit vppcom_service( io_context &ctx );
     ~vppcom_service();
@@ -5,13 +27,23 @@ struct vppcom_service : public io_context::service {
     void start();
     void run_epoll();
 
+    void process_events( uint32_t fd, op_type op );
+
 private:
     boost::scoped_ptr<boost::asio::io_service::work> work;
     boost::asio::io_service async_io_service;
     std::thread thread;
 
+    std::deque<op> scheduled_ops;
+    std::deque<op> incoming_ops;
+
+    std::map<uint32_t,vppcom_listener> listeners;
+    std::map<uint32_t,vppcom_session> sessions;
+    std::map<uint32_t,pkt_queue> ingress_queue;
+    std::map<uint32_t,pkt_queue> egress_queue;
+
     // vppcom
-    int fd;
+    int epoll_fd;
 };
 
 struct vppcom_worker {
@@ -32,8 +64,28 @@ struct vppcom_worker {
     void callback( boost::system::error_code &ec );
 };
 
-struct vppcom_socket {
-    uint32_t socket_handler;
+struct vppcom_listener {
+    uint32_t sock;
+    vppcom_endpt_t local;
+    io_context &io;
 
-    vppcom_socket( uint16_t port );
+    vppcom_listener( io_context &ctx ):
+        io( ctx ) 
+    {}
+
+    void async_accept( accept_handler func );
+};
+
+struct vppcom_session {
+    uint32_t sock;
+    vppcom_endpt_t local;
+    vppcom_endpt_t remote;
+
+    vppcom_session( uint32_t s, vppcom_endpt_t l, vppcom_endpt_t r  ):
+        sock( s ),
+        local( l ),
+        remote( r )
+    {}
+
+    
 };
